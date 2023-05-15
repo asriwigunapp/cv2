4e5f524695deb14c53d5aed6f64e1f2ffb2c3cd8
asriwigunapp <asriwigunap@gmail.com>
2023-05-15 22:01:59 +0700

    first commit

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Curriculum Vitae</title>
+        <!-- Favicon-->
+        <link rel="icon" type="" href="assets/image/user.png" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container px-5">
+                <a class="navbar-brand" href="#!">Curriculum Vitae</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container px-4 px-lg-5">
+            <!-- Heading Row-->
+            <div class="row gx-4 gx-lg-5 align-items-center my-5">
+                <div class="col-lg-7"><img class="img-fluid rounded mb-4 mb-lg-0" src="assets/image/header.png" alt="..." /></div>
+                <div class="col-lg-5">
+                    <h1 class="font-weight-light">Asri Wiguna Pramesti</h1>
+                    <p>Sebagai seorang mahasiswa Teknik Informatika yang berdedikasi dan bersemangat, saya memiliki minat yang mendalam dalam dunia teknologi informasi. Saya percaya bahwa inovasi dan perkembangan teknologi memiliki potensi besar untuk mengubah cara kita hidup dan bekerja. Saya memiliki keterampilan teknis yang solid, serta kemampuan untuk beradaptasi dengan cepat dalam lingkungan yang dinamis.</p>
+                </div>
+            </div>
+            <!-- Call to Action-->
+            <div class="card text-white bg-secondary my-5 py-4 text-center">
+                <div class="card-body"><p class="text-white m-0">asriwigunap@gmail.com  ||  083808692455  ||  Jl. Benteng Kidul, RT/RW 01/01 Kel. Benteng Kec. Warudoyong Kota Sukabumi</p></div>
+            </div>
+            <!-- Content Row-->
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Experience</h2>
+                            <p class="card-text">3 Bulan - Magang di Telkom<br>Selama magang di bidang jaringan, saya memiliki kesempatan untuk mendapatkan pengalaman praktis yang berharga dalam merancang, mengimplementasikan, dan mengelola infrastruktur jaringan. Saya bekerja dengan tim yang berdedikasi dan mendapatkan pemahaman yang mendalam tentang konsep dan prinsip dasar jaringan komputer.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Education</h2>
+                            <p class="card-text">(2020 to Present) Universitas Muhammadiyah Sukabumi <br><br> (2017 to 2020) SMK ABDI BANGSA </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h2 class="card-title">Skill</h2>
+                            <p class="card-text"><b>Soft Skill</b><br>Komunikasi<br>Kerja Tim <br>Beradaptasi<br><br><b>Hard Skill</b><br>Bahasa Pemrograman (PHP, CSS, Java, HTML)<br>Microsoft Office</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container px-4 px-lg-5"><p class="m-0 text-center text-white">&copy; 2023 - Asri Wiguna Pramesti</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
